daa45292e5548e0dfcdd88e08cda6b9cd7b89df2
Darshanmistry1995 <mistrydarshan1995@gamil.com>
2020-03-27 23:30:10 -0400

    Random_num

---
+++ Pthread/Cranum.c
@@ -0,0 +1,30 @@
+//Darshan Mistry
+// C program for generating a random number with having a range. 
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+  
+
+void my_ran(int lower, int higher, int count) 
+           { 
+            int i; 
+            for (i = 0; i < count; i++)
+                { 
+                  int num = (rand() % (higher - lower + 2)) + higher; 
+                      printf("The randum number is %d", num ); 
+                 } 
+                   } 
+  
+                    int main() 
+                    { 
+                      int lower = 5, higher = 7, count = 1; 
+  
+    
+                       srand(time(0)); //using srand function to generate a random number....
+  
+                         my_ran(lower, higher, count); 
+  
+                         return 0; 
+                      } 
+
