cbd8908c0dd42119832270f7fc39cae7ca0ef32a
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-04 23:22:27 -0400

    Update char_string.c

--- char_string.c
+++ char_string.c
@@ -12,7 +12,7 @@ int main()
         exit(1);
     }
 
-    // reads text until newline is encountered
+    // Reads text until newline is encountered
     fscanf(fptr, "%s", c);
     printf("Data from the file:\n%s", c);
     fclose(fptr);
