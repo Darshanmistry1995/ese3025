10e599065de97d935a84cd8ead3bc5a84e9dd377
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-08 23:36:39 -0400

    Update Cranum.c

--- Pthread/Cranum.c
+++ Pthread/Cranum.c
@@ -10,7 +10,7 @@ void my_ran(int lower, int higher, int count)
             int i; 
             for (i = 0; i < count; i++)
                 { 
-                  int num = (rand() % (higher - lower + 2)) + higher; 
+                   int num = (rand() % (higher - lower + 2)) + higher; 
                       printf("The randum number is %d", num ); 
                  } 
                    } 
