b8d224a15932f5cfcb43831d37ac031ad7d7c995
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-14 00:26:16 -0400

    Create LED_Blink.py

---
+++ BBB_GPIO/LED_Blink.py
@@ -0,0 +1,10 @@
+import Adafruit_BBIO.GPIO as GPIO 
+import time //Time def.
+ 
+GPIO.setup("P8_11", GPIO.OUT) //Pin_10 = GPIO_45 Config as a output
+ 
+while True: //while loop
+    GPIO.output("P8_11", GPIO.HIGH)  //for turn on
+    time.sleep(0.5)
+    GPIO.output("P8_11", GPIO.LOW)  //for turn off
+    time.sleep(0.5)
