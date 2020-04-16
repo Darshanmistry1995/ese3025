eabf74f25493d99f562e3ed619280bbe2d4964b2
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-15 23:55:43 -0400

    Argument passing pthread

---
+++ Pthread/Arg_Passing.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct argum 
+
+{
+    char* user_name;
+    int user_age;
+};
+
+void *func(void *input) 
+
+{
+    printf("name: %s\n", ((struct argum*)input)->user_name);
+    printf("age: %d\n", ((struct argum*)input)->user_age);
+}
+
+int main() {
+    struct argum *Darshan = (struct argum *)malloc(sizeof(struct argum));
+    char darshan[] = "Darshan";
+    Darshan->user_name = darshan;
+    Darshan->user_age = 24;
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, func, (void *)Darshan);
+    pthread_join(tid, NULL);
+    return 0;
+}
