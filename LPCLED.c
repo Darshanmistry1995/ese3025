cc2581adea4e7fb7d53f76e7de0f1965bbac1e68
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-04-22 23:59:53 -0400

    Update LPCLED.c

--- LPCLED.c
+++ LPCLED.c
@@ -13,6 +13,8 @@ static void setHrd(void)
 	
 	SystemCoreClockUpdate();
 	Board_Init(); //Initializing a board for setting up....
+	
+
 	//As in a initial state all leds are in off condition..
 
 	Board_LED_Set(0, true);  //RED   LED OFF
